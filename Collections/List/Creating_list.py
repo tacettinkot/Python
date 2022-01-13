92aac24e038337072b627e786f940e107310c7d5
tacettinkot <tacettinkot@gmail.com>
2022-01-12 18:26:35 -0800

    added collection folder and list subject

---
+++ Collections/List/Creating_list.py
@@ -0,0 +1,37 @@
+# [] and list() create list collection
+
+# Example
+"veli" # str variable 
+list("veli") # ['v', 'e', 'l', 'i']
+str = "ali"
+print(list(str)) # ['a', 'l', 'i']
+
+# str variable splits into its chracters and stored in list
+# But if we put into [] then it will accept as an one variable
+
+# Example
+l = ["ali"] 
+print(l) #  ['ali']
+
+list_1 = ['h', 'a', 'p', 'p', 'y']
+word = "happy"
+list_2 = list(word)
+
+print(list_1) # ['h', 'a', 'p', 'p', 'y']
+print(list_2) # ['h', 'a', 'p', 'p', 'y']
+
+country =['USA', 'Brasil', 'UK', 'Germany', 'Turkey', 'New Zealand']
+print(country) # ['USA', 'Brasil', 'UK', 'Germany', 'Turkey', 'New Zealand']
+
+# Example
+
+string_1 = 'I quit smoking'
+new_list_1 = list(string_1) # we created multi element list
+print(new_list_1) # ['I', ' ', 'q', 'u', 'i', 't', ' ', 's', 'm', 'o', 'k', 'i', 'n', 'g']
+
+new_list_2 = [string_1] # this is a single element list
+print(new_list_2) # ['I quit smoking']
+
+
+
+
