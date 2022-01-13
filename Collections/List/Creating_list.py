066f1613e793a4f1b39ef1b5c3db66295ea89eda
tacettinkot <tacettinkot@gmail.com>
2022-01-12 23:20:58 -0800

    Update collection note

--- Collections/List/Creating_list.py
+++ Collections/List/Creating_list.py
@@ -32,6 +32,14 @@ print(new_list_1) # ['I', ' ', 'q', 'u', 'i', 't', ' ', 's', 'm', 'o', 'k', 'i',
 new_list_2 = [string_1] # this is a single element list
 print(new_list_2) # ['I quit smoking']
 
+# Example
+my_list = ['Ali', 'Tom', 2022]
+new_l = list(my_list)
+new_l2 = [my_list]
 
+print(new_l) # ['Ali', 'Tom', 2022]
+print(len(new_l)) # 3
+print(new_l2) # [['Ali', 'Tom', 2022]]
+print(len(new_l2)) # 1
 
 
